decd26ef6903ebd6d136a9a94f1818f3f79a0e80
MuthuMaha <muthumaharajan1992@gmail.com>
2019-05-17 13:20:23 +0530

    third

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,7 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': 'YOUR-SENDER-ID'
+  'apiKey': "AIzaSyDXtmo8yd34xs1Ib-QejASB1SNSppwai3E",
+    'authDomain': "first-project-38df5.firebaseapp.com",
+    'databaseURL': "https://first-project-38df5.firebaseio.com",
+    'projectId': "first-project-38df5",
+    'storageBucket': "first-project-38df5.appspot.com",
+    'messagingSenderId': "1080040497200",
+    'appId': "1:1080040497200:web:9c3a51a0147b7d2a"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
